0b1d74a03cdb1be9ed88ff132671b3d593d881a4
tommsojer <tom.sojer2@gmail.com>
2023-10-24 16:17:27 +0200

    Some refactoring

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   let listGroup = document.querySelector(".list-group");
-  let allItems;
-  let allContent;
-  let activeItemIndex;
+  let allItems, allContent, activeItemIndex;
 
   chrome.storage.local.get(["copiedValues"]).then((result) => {
     for (let i = 0; i < result.copiedValues.length; i++) {
-      let listItem = document.createElement("li");
-      listItem.className = "list-group-item";
+      let listItem = document.createElement("a");
+      listItem.setAttribute("href", "#");
+      listItem.setAttribute("data-bs-toggle", "list");
+      listItem.className = "list-group-item list-group-item-action";
       let listItemHtml = `
         <div class="row">
           <div class="col text-truncate content">${result.copiedValues[i]}</div>
-          <div class="col-auto number">${i}</div>
+          <div class="col-auto" style="border-width: 1px; border-radius: 3px; 
+          border-style: solid;">${i}</div>
         </div>`;
       if (i == 0) {
         listItem.classList.add("active");
@@ -25,40 +26,39 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   document.addEventListener("keydown", (event) => {
-    searchActiveIndex();
+    activeItemIndex = searchActiveIndex(allItems);
 
     if (event.key == "Enter") {
       pasteToContentScript(allContent[activeItemIndex].innerText.trim());
       window.close();
     } else if (
       event.key === "ArrowDown" &&
-      activeItemIndex < allItems.length - 1
+      activeItemIndex < allContent.length - 1
     ) {
       allItems[activeItemIndex].classList.remove("active");
       allItems[activeItemIndex + 1].classList.add("active");
     } else if (event.key === "ArrowUp" && activeItemIndex > 0) {
       allItems[activeItemIndex].classList.remove("active");
       allItems[activeItemIndex - 1].classList.add("active");
-    }
-    if (Array.from("0123456789").includes(event.key)) {
+    } else if (Array.from("0123456789").includes(event.key)) {
+      pasteToContentScript(allContent[event.key].innerText.trim());
       window.close();
     }
   });
 
   listGroup.addEventListener("click", () => {
-    searchActiveIndex();
+    activeItemIndex = searchActiveIndex(allItems);
     pasteToContentScript(allContent[activeItemIndex].innerText.trim());
-
     window.close();
   });
 });
 
-const searchActiveIndex = () => {
-  allItems.forEach((item, index) => {
-    if (item.classList.contains("active")) {
-      activeItemIndex = index;
+const searchActiveIndex = (items) => {
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].classList.contains("active")) {
+      return i;
     }
-  });
+  }
 };
 
 const pasteToContentScript = (content) => {
